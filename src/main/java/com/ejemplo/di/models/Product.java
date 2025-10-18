1d09a9d52fdbe4cead4505d09c391d341b968029
eduardoacunacoronado <edu191191@gmail.com>
2025-10-18 03:00:12 +0200

    Creando la clase Model y la clase Repository.

---
+++ src/main/java/com/ejemplo/di/models/Product.java
@@ -0,0 +1,16 @@
+package com.ejemplo.di.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    private Long id;
+    private String name;
+    private Long price;
+}
---
+++ src/main/java/com/ejemplo/di/repositories/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.ejemplo.di.repositories;
+
+import com.ejemplo.di.models.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepository {
+
+    private List<Product> products;
+
+    public ProductRepository() {
+        this.products = Arrays.asList(
+            new Product(1L, "Corsair", 300L),
+            new Product(2L, "Intel", 850L),
+            new Product(3L, "Razer", 180L),
+            new Product(4L, "Gigabyte", 490L)
+        );
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
