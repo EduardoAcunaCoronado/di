eda6361be7aacc58f166644b41cf0eb69e97180d
eduardoacunacoronado <edu191191@gmail.com>
2025-10-18 08:30:43 +0200

    Optimize imports.

--- src/main/java/com/ejemplo/di/services/ProductService.java
+++ src/main/java/com/ejemplo/di/services/ProductService.java
@@ -1,10 +1,8 @@
 package com.ejemplo.di.services;
 
 import com.ejemplo.di.models.Product;
-import com.ejemplo.di.repositories.ProductRepositoryImpl;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public interface ProductService {
     List<Product> getProducts();
